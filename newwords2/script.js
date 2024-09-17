5a100798cdc533054b58767ed6ee00ac989ecd23
Slamet Prastiyo <93968211+slametprastiyo@users.noreply.github.com>
2024-09-17 12:15:59 +0700

    new words

---
+++ newwords2/script.js
@@ -0,0 +1,99 @@
+// localStorage.removeItem("words");
+const addNew = document.querySelector("#add-new");
+
+
+  const testArea = document.querySelector(".testArea");
+
+  let words = [];
+  getSavedWords();
+  function getSavedWords(){
+    w = localStorage.getItem("words");
+    if(w != null){
+      words = JSON.parse(w);
+    }
+  }
+  // testArea.innerHTML = JSON.stringify(wordStorage);
+  // testArea.innerHTML = JSON.stringify(words);
+  const container = document.querySelector(".container");
+  // container.innerHTML = words[1];
+  if(words != []){
+    loadWords();
+  }
+  function loadWords(){
+    container.innerHTML = "";
+    if(words != []){
+      words.forEach((word)=>{
+    const wordEl  = document.createElement("div");
+    wordEl.classList.add("word");
+  
+  const w = word.split(",");
+  const langEn = document.createElement("div");
+  langEn.classList.add("lang");
+  langEn.classList.add("en");
+  langEn.innerHTML = w[0];
+
+  const langId = document.createElement("div");
+  langId.classList.add("lang");
+  langId.classList.add("id");
+  langId.innerHTML = w[1];
+
+  wordEl.appendChild(langEn);
+  wordEl.appendChild(langId);
+container.appendChild(wordEl);
+  })
+
+  // show ID translation
+  const allWords = document.querySelectorAll(".word");
+  allWords.forEach((word)=>{
+    // testArea.innerHTML = JSON.stringify(word);
+
+    const en = word.querySelector(".en");
+    const id = word.querySelector(".id");
+    en.addEventListener("click",()=>{
+      id.classList.toggle("show");
+      // testArea.innerHTML += "ok";
+    })
+})
+    }
+    
+
+  }
+  
+const form = document.querySelector("form");
+
+const saveButton = document.querySelector(".save-button");
+saveButton.addEventListener("click",()=>{
+  event.preventDefault();
+  let en = form.querySelector("#en");
+en = en.value;
+let id = form.querySelector("#id");
+id = id.value;
+// testArea.innerHTML = en + id;
+if(en != "" && id != ""){
+  saveNewWord(en,id);
+  loadWords();
+  form.classList.toggle("show");
+}
+})
+function saveNewWord(en, id){
+  
+    words.push(en+","+id);
+  
+  // wordStorage = words.join(".");
+
+  localStorage.setItem("words", JSON.stringify(words));
+}
+
+// const addNewBtn = document.querySelector(".add-new");
+// addNewBtn.addEventListener("click",()=>{
+// container.innerHTML ="";
+//   const form = document.querySelector("form");
+//   testArea.innerHTML = "hello";
+//   form.classList.toggle("show");
+// })
+
+addNew.addEventListener("click",()=>{
+// container.innerHTML ="";
+// testArea.innerHTML = addNew;
+form.classList.toggle("show");
+})
